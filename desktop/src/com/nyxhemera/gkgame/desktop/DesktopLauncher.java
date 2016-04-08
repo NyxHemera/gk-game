000cea5e13ad5b8b14630a11d2b37ce2fcc5ba66
Jordan Rust <jordan.rust92@gmail.com>
2016-04-08 15:55:24 -0400

    Initial Commit

---
+++ desktop/src/com/nyxhemera/gkgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nyxhemera.gkgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nyxhemera.gkgame.GKGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GKGame(), config);
+	}
+}
